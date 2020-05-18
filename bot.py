190e9cf40977ebd06567dd897e640102fe108099
Seniorious0 <1054329447@qq.com>
2020-05-18 00:33:07 -0700

    test

--- bot.py
+++ bot.py
@@ -34,7 +34,6 @@ activity_table = json.loads(open(path + 'excel/activity_table.json', 'r', encodi
 old_num = 137
 skin_list = []
 
-print('run:', time.time())
 
 # bot_basic.create_char(se, url, old_num, character_table, skill_table, building_data, item_table, team_table, gamedata_const, stories_table, skin_table, id_table)
 # bot_charword.create_charword(se, url, old_num, character_table, charword_table, id_table)
@@ -49,9 +48,9 @@ print('run:', time.time())
 
 bot_mission.update_mission(se, url, mission_table, item_table)
 bot_activity.update_activity(se, url, activity_table, item_table, building_data, character_table, skin_table)
-bot_charattr.get_char_attr(se, url, character_table, id_table)
-bot_skin.update_skin_handbook(se, url, character_table, skin_table)
-bot_buildingBuff.update_buildingBuff_data(se, url, building_data, character_table, gamedata_const)
+# bot_charattr.get_char_attr(se, url, character_table, id_table)
+# bot_skin.update_skin_handbook(se, url, character_table, skin_table)
+# bot_buildingBuff.update_buildingBuff_data(se, url, building_data, character_table, gamedata_const)
 
 # bot_furni.update_furni(se, url, building_data, item_table)
 # bot_charword.update_charword(se, url, character_table, charword_table)
