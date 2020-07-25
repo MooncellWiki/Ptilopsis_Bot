821df02d4c721a6d8e2b5fc1c61678e6e9730924
Seniorious0 <1054329447@qq.com>
2020-07-25 21:31:45 +0800

    push

---
+++ jobs/daily_rune.py
@@ -0,0 +1,18 @@
+from utils.job import Job
+from utils.richTextStyles import RichTextStyles
+
+
+class Sample(Job):
+    def _run(self):
+        character_table = self.getgd('excel/character_table.json')
+        rts = RichTextStyles(self.getgd('excel/gamedata_const.json'))
+
+        content = ''
+
+        self.wiki.edit(
+            title = '',
+            text = content,
+            summary = 'update'
+        )
+        # print(content)
+        print('Updated: {}.'.format(''))
