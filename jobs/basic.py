03b50a862f14bf642d6d2ff14f3475d1b5c4fee5
Seniorious0 <1054329447@qq.com>
2020-07-28 22:14:16 +0800

    update

--- jobs/basic.py
+++ jobs/basic.py
@@ -795,7 +795,6 @@ class Basic(Job):
                 continue
             if (char_detail['name'] in id_table and old_num >= int(id_table[char_detail['name']]['id'])):
             # if char_detail['name'] not in ['稀音']:
-            # if char_detail['profession'] == 'TRAP' or char_detail['profession'] == 'TOKEN':
                 continue
 
             basic_info = get_basic_info(char_detail, char_key, id_table, stories_table, team_table, skin_table, rts)
