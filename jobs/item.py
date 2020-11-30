73edf17b1944799dd6fb85d4c6d9480bbf22d278
Seniorious0 <1054329447@qq.com>
2020-11-30 13:19:00 +0800

    update

--- jobs/item.py
+++ jobs/item.py
@@ -21,9 +21,9 @@ def build_time(sec):
     return "%02d:%02d:%02d" % (h, m, s)
 
 
-basic_info4 = '==基础信息==\n{{{{道具信息\n|名称={name}\n|描述={description}\n|用途={usage}\n|' \
+basic_info4 = '==基础信息==\n{{{{道具信息\n|名称={name}\n|itemId={itemId}\n|描述={description}\n|用途={usage}\n|' \
               '获得方式={obtainApproach}\n|稀有度={rarity}\n|id={id}\n|分类={sort}\n}}}}\n'
-basic_info3 = '==基础信息==\n{{{{道具信息\n|名称={name}\n|描述={description}\n|用途={usage}\n|' \
+basic_info3 = '==基础信息==\n{{{{道具信息\n|名称={name}\n|itemId={itemId}\n|描述={description}\n|用途={usage}\n|' \
               '稀有度={rarity}\n|id={id}\n|分类={sort}\n}}}}\n'
 basic_mf = '{{{{道具配方/制造站\n|产物={name}\n|产物数量={count}\n|仓库消耗={weight}\n|' \
            '时间消耗={costPoint}\n|制造站等级需求={roomLevel}\n'
@@ -67,6 +67,7 @@ class Item(Job):
             if item_table['items'][item]['obtainApproach']:
                 tbasic_info = basic_info4.format(
                     name=citem['name'].rstrip(),
+                    itemId=citem['itemId'],
                     description=citem['description'],
                     usage=citem['usage'],
                     obtainApproach=citem['obtainApproach'],
@@ -76,6 +77,7 @@ class Item(Job):
             else:
                 tbasic_info = basic_info3.format(
                     name=citem['name'].rstrip(),
+                    itemId=citem['itemId'],
                     description=citem['description'],
                     usage=citem['usage'],
                     rarity=citem['rarity'],
@@ -140,4 +142,4 @@ class Item(Job):
                     tbasic_info = tbasic_info + '==材料掉落=='
             fin = '{{Navigator|道具一览}}\n' + tbasic_info + '\n{{道具导航}}'
             # print(fin)
-            self.wiki.edit(title=citem['name'].rstrip(), text=fin, summary='item init', createonly=True)
+            # self.wiki.edit(title=citem['name'].rstrip(), text=fin, summary='item init', createonly=True)
