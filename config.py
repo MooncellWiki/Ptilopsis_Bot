2d7e44940c846ccf554baeb249ad63476674290c
Seniorious0 <1054329447@qq.com>
2021-03-26 17:18:07 +0800

    update config url

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ config = {
                 "updateMsg": "[JP UPDATE] Client:{0} Data:{1}",
                 "tableSign": True,
                 "luaSign": True,
-                "url": "https://ark-jp-static-online.akamaized.net/assetbundle/official/Android/",
-                "baseUrl": "http://ark-jp-static-online.oss-ap-northeast-1.aliyuncs.com/assetbundle/official/Android/",
+                "url": "https://ark-jp-static-online.yo-star.com/assetbundle/official/Android/",
+                "baseUrl": "http://ark-jp-static-online.yo-star.com/assetbundle/official/Android/",
                 "chatMask": "8OjXUSNSi8yXC0u98mNWvh7MRLGhyEuQ"
             },
             "us": {
@@ -29,8 +29,8 @@ config = {
                 "updateMsg": "[EN UPDATE] Client:{0} Data:{1}",
                 "tableSign": True,
                 "luaSign": True,
-                "url": "https://ark-us-static-online.akamaized.net/assetbundle/official/Android/",
-                "baseUrl": "http://ark-us-static-online.arknights.global/assetbundle/official/Android/",
+                "url": "https://ark-us-static-online.yo-star.com/assetbundle/official/Android/",
+                "baseUrl": "http://ark-us-static-online.yo-star.com/assetbundle/official/Android/",
                 "chatMask": "8OjXUSNSi8yXC0u98mNWvh7MRLGhyEuQ"
             },
             "kr": {
@@ -38,8 +38,8 @@ config = {
                 "updateMsg": "[KR UPDATE] Client:{0} Data:{1}",
                 "tableSign": True,
                 "luaSign": True,
-                "url": "https://ark-kr-static-online-1300509597.akamaized.net/assetbundle/official/Android/",
-                "baseUrl": "http://ark-kr-static-online-1300509597.cos.ap-seoul.myqcloud.com/assetbundle/official/Android/",
+                "url": "https://ark-kr-static-online-1300509597.yo-star.com/assetbundle/official/Android/",
+                "baseUrl": "http://ark-kr-static-online-1300509597.yo-star.com/assetbundle/official/Android/",
                 "chatMask": "8OjXUSNSi8yXC0u98mNWvh7MRLGhyEuQ"
             },
             "tw": {
@@ -48,7 +48,7 @@ config = {
                 "tableSign": False,
                 "luaSign": True,
                 "url": "https://akcdn.imtxwy.com/assetbundle/official/Android/",
-                "baseUrl": "",
+                "baseUrl": "http://akcdn.imtxwy.com/assetbundle/official/Android/",
                 "chatMask": "8OjXUSNSi8yXC0u98mNWvh7MRLGhyEuQ"
             }
         }
