d311e1e875d9cd070579e94286367300d9451841
Seniorious0 <1054329447@qq.com>
2021-03-30 08:48:57 +0800

    update

--- test.py
+++ test.py
@@ -1,33 +1,25 @@
-import sys
 import json
-from config import config
-from utils.data import GameData
-from utils.wiki import Wiki
-from utils.unpacker import Unpacker
+import sys
 
+from config import config
+# from jobs._temp import Temp
+# from jobs.basic import Basic
+# from jobs.charword import Charword
 # from jobs.route import Route
-# from jobs.gacha import Gacha
-# from jobs.crisis import Crisis
-# from jobs.stage import Stage
-# from jobs.weedy import Weedy
 # from jobs.sidebar import Sidebar
 # from jobs.skin import Skin
-from jobs.stage import Stage
-# from jobs.rogue_stage import RougeStage
-from jobs._temp import Temp
-from jobs.basic import Basic
-# from jobs.charword import Charword
-
-
+# from jobs.stage import Stage
+# from jobs.weedy import Weedy
+from utils.data import GameData
+from utils.unpacker import Unpacker
+from utils.wiki import Wiki
 
 if __name__ == '__main__':
     wiki = Wiki(config['api_url'], config['username'], config['password'],
                 ('dev' if '-dev' in sys.argv else 'product'))
-    # gameData = GameData(config = config, source = 'ArknightsGameData')
-    # gameData = GameData(config = config, source = 'UnpackerData')
-    gameData = GameData(config = config, source = 'Unpacker')
+    gameData = GameData(config=config, source='Unpacker')
 
-    # with open('/Users/Josiah/Desktop/Arknight/PRTS_bot/UnpackerData/levels/obt/rune/level_rune_04-01.json', 'r', encoding = 'utf-8') as f:
+    # with open('/Users/Josiah/Desktop/Arknight/PRTS_bot/UnpackerData/levels/obt/rune/level_rune_04-01.json', 'r', encoding='utf-8') as f:
     #     print(Stage(wiki, gameData)._run_enemy_data(json.loads(f.read())))
 
     # Route(wiki, gameData).run()
@@ -43,9 +35,8 @@ if __name__ == '__main__':
     # Basic(wiki, gameData)._run_update(old_num=178)
     # Charword(wiki, gameData)._run_update_jp()
 
-    # up = UnpackerCN(config['unpacker'])
-    # up.get_all_gamedata()
-    # up.unpack_all_gamedata()
+    # up = Unpacker(config['unpacker'])
+    # up.get_all_ab()
+    # up.unpack_all_data()
     # up.get_ab("battle/prefabs/[uc]skills.ab")
     # up.unpack_data("battle/prefabs/[uc]skills.ab")
-
