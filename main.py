90e8a1c9cdb782abe917158056568b4e3f809b4f
Seniorious0 <1054329447@qq.com>
2022-08-25 17:07:06 +0800

    update

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         Stage(wiki, gameData).run_memory()  # 悖论模拟
         Stage(wiki, gameData).run_campaign()  # 剿灭
         # Stage(wiki, gameData).run_crisis()  # 需crisis_info
-        # Stage(wiki, gameData).run_id('levels/obt/legion')
+        # Stage(wiki, gameData).run_id('levels/obt/rune/level_rune_12-01.json')
         Charword(wiki, gameData).update()
         
         # from jobs.route import Route
