d63de649db4c446709b94b0b5a7e2d04329366a2
Seniorious0 <1054329447@qq.com>
2022-11-15 18:41:35 +0800

    update

--- jobs/stage.py
+++ jobs/stage.py
@@ -1000,10 +1000,14 @@ class Stage(Job):
         # https://weedy.baka.icu/crisis/today/internal
         with open('crisis_info.json', 'r', encoding='utf-8') as file:
             stage_table = json.loads(file.read())
-        for stage_key in stage_table['data']['seasonInfo'][0]['stages']:
-            stage_detail = stage_table['data']['seasonInfo'][0]['stages'][stage_key]
-            stage_detail['stageId'] = stage_key
-            stage_detail['levelId'] = 'Obt/rune/' + stage_key
+        # for stage_key in stage_table['data']['seasonInfo'][0]['stages']:
+        #     stage_detail = stage_table['data']['seasonInfo'][0]['stages'][stage_key]
+        #     stage_detail['stageId'] = stage_key
+        #     stage_detail['levelId'] = 'Obt/rune/' + stage_key
+        for stage_x in stage_table['stages']:
+            stage_detail = stage_x
+            stage_detail['stageId'] = stage_detail['id']
+            stage_detail['levelId'] = 'Obt/rune/' + stage_detail['id']
 
         # 从 weedy 读
         # session = requests.Session()
