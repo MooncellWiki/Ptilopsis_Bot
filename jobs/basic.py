cd375565964ab22326c8d6abcb32850a9dc29a53
Seniorious0 <1054329447@qq.com>
2024-03-18 20:15:42 +0800

    update

--- jobs/basic.py
+++ jobs/basic.py
@@ -68,11 +68,11 @@ def get_basic_info(char_detail, char_key, id_table, rts, uniequip_table, team_ta
         lang_dict['CN_MANDARIN'], lang_dict['CN_TOPOLECT'] = '中文', '中文方言'
         for k in cv_dict:
             lang = lang_dict.get(k, '未知语言')
-            if lang == '联动':
-                if char_key in ['char_4019_ncdeer']:
-                    lang = '中文'
-                elif char_key in ['char_456_ash', 'char_458_rfrost', 'char_457_blitz', 'char_459_tachak', 'char_4123_ela', 'char_4124_iana', 'char_4125_rdoc', 'char_4126_fuze']:
-                    lang = '英文'
+            # if lang == '联动':
+            #     if char_key in ['char_4019_ncdeer']:
+            #         lang = '中文'
+            #     elif char_key in ['char_456_ash', 'char_458_rfrost', 'char_457_blitz', 'char_459_tachak', 'char_4123_ela', 'char_4124_iana', 'char_4125_rdoc', 'char_4126_fuze']:
+            #         lang = '英文'
             basic_info += f"\n|{lang}配音={','.join(cv_dict[k]['cvName'])}"
     except:
         basic_info += '\n|日文配音='
@@ -1318,11 +1318,11 @@ class Basic(Job):
                 lang_dict['CN_MANDARIN'], lang_dict['CN_TOPOLECT'] = '中文', '中文方言'
                 for k in cv_dict:
                     lang = lang_dict.get(k, '未知语言')
-                    if lang == '联动':
-                        if char_key in ['char_4019_ncdeer']:
-                            lang = '中文'
-                        elif char_key in ['char_456_ash', 'char_458_rfrost', 'char_457_blitz', 'char_459_tachak']:
-                            lang = '英文'
+                    # if lang == '联动':
+                    #     if char_key in ['char_4019_ncdeer']:
+                    #         lang = '中文'
+                    #     elif char_key in ['char_456_ash', 'char_458_rfrost', 'char_457_blitz', 'char_459_tachak', 'char_4123_ela', 'char_4124_iana', 'char_4125_rdoc', 'char_4126_fuze']:
+                    #         lang = '英文'
                     cv += f"\n|{lang}配音={','.join(cv_dict[k]['cvName'])}"
             except:
                 cv += '\n|日文配音='
